32f0171e92e10585d69e5918c9251c506599207b
Giovanni Moscato <giovi94@gmail.com>
2021-03-23 22:27:14 +0100

    Addendum

--- src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Call_1 = require("./Call");
-console.log('Running');
-Call_1.Call({ 'token': '123' }).subscribe(console.log, console.error);
-//# sourceMappingURL=index.js.map
